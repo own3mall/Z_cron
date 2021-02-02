ae7987c6b1c14f139d6e3da740d0fcbd3468859d
OwN-3m-All <own3mall@gmail.com>
2021-02-01 20:29:26 -0700

    Slimmed Down for Photon Q Running CarbonROM 4.4.4

--- Z_cron/cronsetup.sh
+++ Z_cron/cronsetup.sh
@@ -7,13 +7,13 @@ dir=$(dirname -- "$0");
 ### Make proper directives ###
 
 mkdir /etc/cron.d/;
+mkdir /etc/cron.d/daily;
+mkdir /etc/cron.d/hourly;
 
 ### Move files ###
 
 cp $dir/cron /etc/init.d;
-cp -R $dir/hourly /etc/cron.d/hourly;
 cp -R $dir/daily /etc/cron.d/daily;
-cp -R $dir/minutely /etc/cron.d/minutely;
 cp -R $dir/crontabs /etc/cron.d/crontabs;
 
 ### Fix permissions ###
